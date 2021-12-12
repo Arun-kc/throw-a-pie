ec07a8c2e3d1d26b53235c7e2c4b3f621ab01dab
Arun KC <arunkc91@gmail.com>
2021-12-12 12:15:09 +0530

    feat: update contract

--- contracts/PiePortal.sol
+++ contracts/PiePortal.sol
@@ -6,13 +6,69 @@ import "hardhat/console.sol";
 contract PiePortal {
     uint256 totalPies;
 
-    constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+    uint256 private seed;
+
+    event NewPie(address indexed from, uint256 timestamp, string message);
+
+    struct Pie {
+        address thrower; // The address of the user who throwed a pie.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user throwed the pie.
     }
 
-    function pie() public {
+    Pie[] pies;
+
+    mapping(address => uint256) public lastThrowedAt;
+
+    constructor() payable {
+        console.log("Yo yo, I am a contract and I am smart");
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+    
+
+    function pie(string memory _message) public {
+
+        require(
+            lastThrowedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastThrowedAt[msg.sender] = block.timestamp;
+
+
         totalPies += 1;
         console.log(unicode"%s has thrown a pie! 🥧", msg.sender);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+
+        pies.push(Pie(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        
+        console.log("Random # generated: %d", seed);
+
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewPie(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllPies() public view returns (Pie[] memory) {
+        return pies;
     }
 
     function getTotalPies() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,24 +1,54 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const pieContractFactory = await hre.ethers.getContractFactory('PiePortal');
-  const pieContract = await pieContractFactory.deploy();
+  const pieContract = await pieContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  });
   await pieContract.deployed();
-
   console.log("Contract deployed to:", pieContract.address);
-  console.log("Contract deployed by:", owner.address);
+
+  /*
+   * Get Contract balance
+   */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    pieContract.address
+  );
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   let pieCount;
   pieCount = await pieContract.getTotalPies();
-  
-  let pieTxn = await pieContract.pie();
-  await pieTxn.wait();
+  console.log(pieCount.toNumber());
+
+  /**
+   * Let's throw some pies!
+   */
+  let pieTxn = await pieContract.pie('This is wave #1');
+  await pieTxn.wait(); // Wait for the transaction to be mined
+
+  let pieTxn2 = await pieContract.pie('This is wave #2');
+  await pieTxn2.wait(); // Wait for the transaction to be mined
+
+  /*
+   * Get Contract balance to see what happened!
+   */
+  contractBalance = await hre.ethers.provider.getBalance(pieContract.address);
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  pieTxn = await pieContract.connect(randomPerson).pie('Another message!');
+  await pieTxn.wait(); // Wait for the transaction to be mined
+
+  let allPies = await pieContract.getAllPies();
+  console.log(allPies);
 
   pieCount = await pieContract.getTotalPies();
+  console.log(pieCount.toNumber());
 
-  pieTxn = await pieContract.connect(randomPerson).pie();
-  await pieTxn.wait();
-
-  waveCount = await pieContract.getTotalPies();
 };
 
 const runMain = async () => {
